173fdb48f886771ad3e2682ba06cf4d793b5faf4
FadyF <40200736+FadyF@users.noreply.github.com>
2018-07-28 15:27:24 +0200

    Update media.py

--- media.py
+++ media.py
@@ -12,8 +12,10 @@ class Movie():
         VALID_RATINGS = ["G", "PG", "PG-13", "R"]
         # init method is a constructor that initializes the data.
         # self is the instance.
-	def __init__(self, movie_title, movie_duration, movie_release_date, movie_storyline,movie_director, poster_image,
-                 trailer_youtube, movie_rating):
+
+        def __init__(self, movie_title, movie_duration, movie_release_date,
+                     movie_storyline, movie_director, poster_image,
+                     trailer_youtube, movie_rating):
                 self.title = movie_title
                 self.duration = movie_duration
                 self.release_date = movie_release_date
@@ -22,11 +24,10 @@ class Movie():
                 self.poster_image_url = poster_image
                 self.trailer_youtube_url = trailer_youtube
                 self.rating = movie_rating
-                
 
-        # show _trailer method will show the trailer from youtube link of movie class
+        # show _trailer method will show the trailer from youtube link of movie
+        # class
         # input is self(instance).
 
         def show_trailer(self):
                 webbrowser.open(self.trailer_youtube_url)
-
